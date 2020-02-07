33ea0edcbf7b1e62ce25809ca662a3164d54add4
Patrick Kille <pr.kille@gmail.com>
2020-02-07 09:06:22 -0800

    correct name issues in package.json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'haiku',
+      title: 'Super Galactic Age Calculator',
       template: './src/index.html',
       inject: 'body'
     })
